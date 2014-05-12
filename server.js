dc9c61efa14539d90591bec91f61abd45c282c50
Alex <alex.fedorov1991@gmail.com>
2014-05-12 03:00:48 +0300

    Some changing

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ var morgan  = require('morgan')
 // Компактный вид лога
 app.use(morgan('short'));
 
-app.listen(3000);
-console.log('Listening localhost on port 3000 ...');
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening localhost on ' + port);  
+});
+
 
 // Установка максимального размера JSON файла
 app.use(bodyParser.json({limit: '10mb'}));
